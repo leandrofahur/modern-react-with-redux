c45285eeacfe6ac0cd4f89b34219a694b5565494
leandrofahur <leandrofahur@gmail.com>
2020-09-25 20:56:42 -0700

    my first component

---
+++ jsx/src/index.js
@@ -0,0 +1,11 @@
+// Import libraries such as React and ReactDOM:
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Create a React component
+const App = function () {
+  return <div>Hi there!</div>;
+};
+
+// Take the React component and show it on the screen
+ReactDOM.render(<App />, document.getElementById("root"));
