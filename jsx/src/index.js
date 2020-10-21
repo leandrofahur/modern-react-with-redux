e3a8ffd7a6b4c8da1c762dd49e2d81f2f5a9e848
leandrofahur <leandrofahur@gmail.com>
2020-10-21 11:53:46 -0700

    html with css style inside the JSX

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,7 +4,17 @@ import ReactDOM from "react-dom";
 
 // Create a React component
 const App = function () {
-  return <div>Hi there!</div>;
+  return (
+    <div>
+      <label class="label" for="name">
+        Enter name:{" "}
+      </label>
+      <input id="name" type="text"></input>
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        Submit
+      </button>
+    </div>
+  );
 };
 
 // Take the React component and show it on the screen
