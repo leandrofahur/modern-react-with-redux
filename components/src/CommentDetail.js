83191634b5cbdc038ae133600c6d71a0894a79d3
leandrofahur <leandrofahur@gmail.com>
2020-10-25 14:53:59 -0700

    props in children

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,12 +1,11 @@
 import React from "react";
-import faker from "faker";
 
 const CommentDetail = (props) => {
   // console.log(props);
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={props.authorImg} />
+        <img alt="avatar" src={props.avatar} />
       </a>
       <div className="content">
         <a href="/" className="author">
--- components/src/index.js
+++ components/src/index.js
@@ -1,29 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        authorImg={faker.image.avatar()}
-        author="Alex"
-        time="Today at 7:00PM"
-        comment="Wtf guys..."
-      />
-      <CommentDetail
-        authorImg={faker.image.avatar()}
-        author="Jane"
-        time="Today at 6:00PM"
-        comment="World!"
-      />
-      <CommentDetail
-        authorImg={faker.image.avatar()}
-        author="Sam"
-        time="Yesterday at 5:00AM"
-        comment="Hello"
-      />
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Alex"
+          time="Today at 7:00PM"
+          comment="Wtf guys..."
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Jane"
+          time="Today at 6:00PM"
+          comment="World!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Sam"
+          time="Yesterday at 5:00AM"
+          comment="Hello"
+        />
+      </ApprovalCard>
     </div>
   );
 };
