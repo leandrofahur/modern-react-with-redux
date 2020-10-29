c99cbccfec8e82075f60f6ff398403f1b8184bc0
leandrofahur <leandrofahur@gmail.com>
2020-10-29 16:02:26 -0700

    using SeasonDisplay passing it with props

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // This is the only time we do direct assignmet to this.state
-    this.state = { lat: null, errorMessage: "" };
-  }
+  state = { lat: null, errorMessage: "" };
 
   componentDidMount() {
     // Data loading:
@@ -35,7 +31,7 @@ class App extends React.Component {
       return <div>Error: {this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading!</div>;
