f2fdc9097be0cb6818d4bd6f2e5232f363b6fe09
leandrofahur <leandrofahur@gmail.com>
2020-10-29 17:58:02 -0700

    spinner

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
@@ -34,7 +35,11 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading!</div>;
+    return (
+      <div>
+        <Spinner />
+      </div>
+    );
   }
 }
 
