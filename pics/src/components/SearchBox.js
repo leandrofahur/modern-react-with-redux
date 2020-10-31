f789811cd8ec59cde95170538df6a3afbcd5bef2
leandrofahur <leandrofahur@gmail.com>
2020-10-30 21:52:10 -0700

    SearchBox controlled component

--- pics/src/components/SearchBox.js
+++ pics/src/components/SearchBox.js
@@ -2,13 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text"></input>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => {
+                this.setState({ term: e.target.value });
+              }}
+            />
           </div>
         </form>
       </div>
