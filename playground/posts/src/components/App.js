0d3597897b5c7191ada13126fe0fd07d1ab4d733
leandrofahur <leandrofahur@gmail.com>
2020-11-02 17:52:55 -0800

    simple component hard coded

--- playground/posts/src/components/App.js
+++ playground/posts/src/components/App.js
@@ -1,8 +1,30 @@
+// React libs:
 import React from "react";
-import ReactDOM from "react-dom";
+
+// API's:
+import faker from "faker";
+
+// Styles:
+import "./App.css";
 
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <div className="ui card">
+      <div className="content">
+        <div className="header">Post</div>
+      </div>
+      <div className="content">
+        <h4 className="ui sub header">User name</h4>
+        <input type="text" />
+        <h4 className="ui sub header">Message</h4>
+        <textarea />
+      </div>
+      <div className="ui buttons">
+        <div className="ui green button">Send</div>
+        <div className="ui red button">Cancel</div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
