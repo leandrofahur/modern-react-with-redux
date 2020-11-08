83ba20377a8b10ed260b1df4c9a60e3fab694dbb
leandrofahur <leandrofahur@gmail.com>
2020-11-08 13:14:13 -0800

    api request with promisses

--- playground/react-apis/src/components/App.js
+++ playground/react-apis/src/components/App.js
@@ -1,8 +1,21 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
+  state = { res: [] };
+
+  componentDidMount() {
+    axios
+      .get("https://randomuser.me/api/")
+      // .then((response) => console.log(response.data.results[0].gender));
+
+      .then((response) => {
+        this.setState({ res: response.data.results[0].gender });
+      });
+  }
+
   render() {
-    return <div>Hello from React app</div>;
+    return <div>Gender: {this.state.res}</div>;
   }
 }
 
