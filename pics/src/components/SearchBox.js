3d1304d5f46205083c7762d3e1aa2396336b0866
leandrofahur <leandrofahur@gmail.com>
2020-11-07 16:03:04 -0800

    event handler fixed

--- pics/src/components/SearchBox.js
+++ pics/src/components/SearchBox.js
@@ -4,20 +4,15 @@ import ReactDOM from "react-dom";
 class SearchBar extends React.Component {
   state = { term: "" };
 
-  onFormSubmit(event) {
+  onFormSubmit = (event) => {
     event.preventDefault();
     console.log(this.state.term);
-  }
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form
-          onSubmit={(event) => {
-            this.onFormSubmit(event);
-          }}
-          className="ui form"
-        >
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
