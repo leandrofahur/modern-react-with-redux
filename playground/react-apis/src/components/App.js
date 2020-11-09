918e2cde690360d627b5ad1ebdcf4786f502d95e
leandrofahur <leandrofahur@gmail.com>
2020-11-08 16:50:05 -0800

    simple display of the information received from the API

--- playground/react-apis/src/components/App.js
+++ playground/react-apis/src/components/App.js
@@ -2,20 +2,42 @@ import React from "react";
 import axios from "axios";
 
 class App extends React.Component {
-  state = { res: [] };
+  state = {
+    name: "",
+    gender: "",
+    country: "",
+    image: "",
+  };
 
   onButtonClick = async () => {
     const response = await axios.get("https://randomuser.me/api/");
-    this.setState({ res: response.data.results[0].gender });
+    this.setState({
+      name:
+        response.data.results[0].name.first +
+        " " +
+        response.data.results[0].name.last,
+      gender: response.data.results[0].gender,
+      country: response.data.results[0].location.country,
+      image: response.data.results[0].picture.large,
+    });
+
+    // this.setState({ res: response.data.results[0] });
   };
 
   render() {
     return (
       <div on>
-        Gender: {this.state.res}
+        Name: {this.state.name}
+        <br />
+        Gender: {this.state.gender}
+        <br />
+        Country: {this.state.country}
+        <br />
+        <img src={this.state.image} />
+        <br />
         <br />
         <button onClick={this.onButtonClick} type="submit">
-          Submit
+          Search
         </button>
       </div>
     );
