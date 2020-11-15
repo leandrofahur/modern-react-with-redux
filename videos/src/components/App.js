49d8ba4b034945b36ef24570d16f3bfd00d14ab8
leandrofahur <leandrofahur@gmail.com>
2020-11-15 14:11:55 -0800

    api request to youtube

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,8 +1,24 @@
 import React from "react";
+import youtube from "../apis/youtube";
+import SearchBar from "./SearchBar";
 
 class App extends React.Component {
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+
+    console.log(response.data);
+  };
+
   render() {
-    return <div>App Component</div>;
+    return (
+      <div className="ui container">
+        <SearchBar onTermSubmit={this.onTermSubmit} />
+      </div>
+    );
   }
 }
 
