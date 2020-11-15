bf00eeceea8778170bf65b0177da63fe45187bae
leandrofahur <leandrofahur@gmail.com>
2020-11-15 15:19:08 -0800

    video detail

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import youtube from "../apis/youtube";
 import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
       params: {
@@ -10,13 +14,23 @@ class App extends React.Component {
       },
     });
 
-    console.log(response.data);
+    this.setState({ videos: response.data.items });
+  };
+
+  onVideoSelect = (video) => {
+    // console.log("From the App!", video);
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
