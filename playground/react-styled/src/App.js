bca9a71b0047ef0da35c0a8f2482b044e4ac4d03
leandrofahur <leandrofahur@gmail.com>
2020-12-25 08:10:17 -0800

    update

---
+++ playground/react-styled/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Input from "./components/Input";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <div className='ui container'>
+        <Input></Input>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default App;
---
+++ playground/react-styled/src/components/Input.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <React.Fragment>
+      <div className='ui input'>
+        <input type='text' placeholder='Search'></input>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Input;
